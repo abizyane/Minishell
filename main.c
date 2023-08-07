e4fe48feefca2c536a76bb10177c948d5525a811
ItsAchraf <115986776+abizyane@users.noreply.github.com>
2023-08-07 21:38:48 +0100

    ...

--- main.c
+++
@@ -1,9 +0,0 @@
-#include "minishell.h"
-
-int main(int argc, char const *argv[], char **envp)
-{
-    char *str[2] = {"./script", "\0"};
-    if (execve(str[0], str, envp) == -1)
-        printf("failed");
-    return 0;
-}
