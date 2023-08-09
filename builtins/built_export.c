9a9514a45c5a5f67817a7cb8eaf6cc826e8f4302
ItsAchraf <115986776+abizyane@users.noreply.github.com>
2023-08-09 06:57:04 +0100

    done .

--- builtins/built_export.c
+++ builtins/built_export.c
@@ -6,7 +6,7 @@
 /*   By: abizyane <abizyane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 02:27:02 by ahamrad           #+#    #+#             */
-/*   Updated: 2023/08/09 05:40:31 by abizyane         ###   ########.fr       */
+/*   Updated: 2023/08/09 06:56:36 by abizyane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,8 +45,11 @@ void	print_list(t_env *env)
         }
         if (min) 
 		{
-            printf("declare -x %s=\"%s\"\n", min->key, min->content);
-            min->p_flag = 1;
+			if (min->content)
+            	printf("declare -x %s=\"%s\"\n", min->key, min->content);
+            else
+            	printf("declare -x %s\n", min->key);
+			min->p_flag = 1;
         }
         curr_env = curr_env->nxt;
     }
@@ -72,6 +75,28 @@ int	check_syntax(char *new_var)
 	return (i);
 }
 
+void	update_var(t_env **env, char **new_var)
+{
+	t_env	*var;
+	
+	var = find_env(*env, new_var[0]);
+	if (var->content)
+	{	
+		free (var->content);
+		var->content = NULL;
+		if (new_var[1])
+			var->content = new_var[1];
+		return ;
+	}
+	else 
+	{
+		if (new_var[1])
+			var->content = ft_strdup(new_var[1]);
+		else
+			var->content = ft_strdup("");
+	}
+}
+
 void	add_var(t_env **env, char *new_var)
 {
 	t_env	*tmp;
@@ -88,14 +113,15 @@ void	add_var(t_env **env, char *new_var)
 	j = ++i;
 	while (new_var[j - 1] == '=' && new_var[i])
 		i++;
-	str[1] = ft_substr(new_var, j, i);
-	// tmp = find_env(env, str[0]);
-	// if (find_env(env, str[0]))
-	// {
-	// 	//TODO:update the variable;
-	// 	//TODO:export asdas=;
-	// }
-	env_add_back(env, str);
+	if (new_var[j - 1] == '=' && new_var[j])
+		str[1] = ft_substr(new_var, j, i);
+	else if (new_var[j - 1] == '=' && !new_var[i])
+		str[1] = NULL;
+	tmp = find_env(*env, str[0]);
+	if (tmp)
+		update_var(env, str);
+	else
+		env_add_back(env, str);
 	free(str);
 }
 
