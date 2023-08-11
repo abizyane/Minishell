2d563de216ec664be2da6e9e67d4c2d593afe110
Achraf Bizyane <abizyane@e1r9p9.1337.ma>
2023-08-11 11:20:20 +0100

    still needs heredoc signal solution

--- src/ft_heredoc.c
+++ src/ft_heredoc.c
@@ -14,48 +14,48 @@
 
 void    *freeptr(char **s)
 {
-    if (*s)
-    {
-        free (*s);
-        *s = NULL;
-    }
-    return (*s);
+	if (*s)
+	{
+		free (*s);
+		*s = NULL;
+	}
+	return (*s);
 }
 
 void    handler(int sig)
 {
-    (void)sig;
-    rl_on_new_line();
-    rl_replace_line("", 0);
-    g_rl = 1;
-    ioctl(0, TIOCSTI, "\4");
+	(void)sig;
+	g_rl = 1;
+	rl_on_new_line();
+	ioctl(0, TIOCSTI, "\4");
+	rl_replace_line("", 0);
 }
 
 int    read_heredoc(t_redir *redir, t_env *env)
 {
-    char    *line;
-    char    *dlm;
-    int        fd[2];
+	char    *line;
+	char    *dlm;
+	int        fd[2];
 
-    (void)env;
-    dlm = redir->filename;
-    if (pipe(fd) == -1)
-        return (perror("minishell"), -1);
-    signal(SIGINT, handler);
-    while (1)
-    {
-        line = readline("> ");
-        if (!line || !ft_strcmp(line, dlm))
-            break ;
-        if (redir->heredoc_flag == 0)
-            line = expand_vars(line, env);
-        write(fd[1], line, ft_strlen(line));
-        write(fd[1], "\n", 1);
-        freeptr(&line);
-    }
-    freeptr(&line);
-    close(fd[1]);
-    return (fd[0]);
+	(void)env;
+	dlm = redir->filename;
+	if (pipe(fd) == -1)
+		return (perror("minishell"), -1);
+	signal(SIGINT, handler);
+	while (1)
+	{
+		line = readline("> ");
+		if (!line || !ft_strcmp(line, dlm))
+			break ;
+		if (redir->heredoc_flag == 0)
+			line = expand_vars(line, env);
+		write(fd[1], line, ft_strlen(line));
+		write(fd[1], "\n", 1);
+		freeptr(&line);
+	}
+	freeptr(&line);
+	close(fd[1]);
+	return (fd[0]);
 }
 
 void	close_heredoc_fds(t_cmdline *cmd)
@@ -79,24 +79,24 @@ void	close_heredoc_fds(t_cmdline *cmd)
 
 void    open_heredoc(t_cmdline **head, t_env *env)
 {
-    t_cmdline    *cmd;
-    t_redir        *redir;
+	t_cmdline    *cmd;
+	t_redir        *redir;
 
-    cmd = *head;
-    while (cmd)
-    {
-        redir = cmd->redir;
-        while (redir)
-        {
-            if (redir->type == Heredoc && g_rl == 0)
-            {
-                redir->fd = read_heredoc(redir, env);
-                if (redir->fd == -1)
-                    return ;
-            }
-            redir = redir->nxt;
-        }
-        cmd = cmd->nxt;
-    }
-    g_rl = 0;
+	cmd = *head;
+	while (cmd)
+	{
+		redir = cmd->redir;
+		while (redir)
+		{
+			if (redir->type == Heredoc && g_rl == 0)
+			{
+				redir->fd = read_heredoc(redir, env);
+				if (redir->fd == -1)
+					return ;
+			}
+			redir = redir->nxt;
+		}
+		cmd = cmd->nxt;
+	}
+	g_rl = 0;
 }
